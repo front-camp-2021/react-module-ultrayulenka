e34cb1b5f6f54d373cdfc1e9f1c385661717bcf1
ultrayulenka <cherevata.jul@gmail.com>
2021-10-19 18:55:52 +0300

    Added better navigation rendering logic

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -3,42 +3,36 @@ import React from 'react';
 import { useLocation } from "react-router-dom";
 import NavigationLink from '../navigation-link';
 
+import { startWithCapitalLetter } from '../../utils/startWithCapitalLetter'
+
 import './navigation.scss';
 import home from './home.svg';
 
 export default function Header () {
     
     const { pathname } = useLocation();
+    const paths = pathname.split('/');
 
     return (
         <nav className="nav">
             <NavigationLink 
-                to="/"
-                isCurrent={pathname.endsWith('/') || pathname === '/not-found'}>
-                <div className="home-icon">
-                    <img src={home} alt="home"/>
-                </div>  
+                    to="/"
+                    isCurrent={pathname.endsWith('/') || pathname === '/not-found'}>
+                    <div className="home-icon">
+                        <img src={home} alt="home"/>
+                    </div>  
             </NavigationLink>
             {
-                pathname.includes('electronics')?
-                <NavigationLink 
-                    to="/electronics"
-                    isCurrent={pathname.endsWith('electronics')}>
-                    Electronics
-                </NavigationLink>
-                : pathname.includes('wishlist')? 
-                <NavigationLink         
-                    to="/wishlist"
-                    isCurrent={pathname.endsWith('wishlist')}>
-                    Wishlist
-                </NavigationLink>
-                : pathname.includes('cart')? 
-                <NavigationLink         
-                    to="/cart"
-                    isCurrent={pathname.endsWith('cart')}>
-                    Cart
-                </NavigationLink>
-                : null
+                paths.map((path, index, paths) => {
+                    if(!path || path === 'not-found') return;
+                    return (
+                    <NavigationLink 
+                        to={`/${path}`}
+                        key={path}
+                        isCurrent={index === paths.length - 1}>
+                        {startWithCapitalLetter(path)}
+                    </NavigationLink>);
+                })
             }
         </nav>
     )
